ebc37923da69e4f360b1c5616b138e4386f252a4
Maylo Daring Parra Aguirre <162264699+Mayloparra24@users.noreply.github.com>
2025-10-20 23:29:50 -0600

    nuevos cambios y correciones con los Test

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -18,7 +18,7 @@ export default function Login() {
       // Guardar token en localStorage
       localStorage.setItem("token", res.data.token);
       navigate("/dashboard");
-    } catch (_err: unknown) {
+    } catch {
       setError("Credenciales inválidas");
     }
   };
