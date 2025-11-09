f880d2b7da088e98f59ecb26ad08e37612854988
BRYAN.CALDERONESPINOZA <BRYAN.CALDERONESPINOZA@UCR.AC.CR>
2025-11-09 06:13:30 -0600

    test de recordatorio notificacion

--- tests/Unit/Notifications/RecordatorioPresentarDeclaracionTest.php
+++ tests/Unit/Notifications/RecordatorioPresentarDeclaracionTest.php
@@ -5,41 +5,40 @@ namespace Tests\Unit\Notifications;
 use App\Notifications\RecordatorioPresentarDeclaracion;
 use Carbon\Carbon;
 use Illuminate\Notifications\Messages\MailMessage;
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
 
 class RecordatorioPresentarDeclaracionTest extends TestCase
 {
-    public function test_can_generate_mail_message_with_fecha_limite(): void
+    public function test_puede_generar_mensaje_de_correo_con_fecha_limite(): void
     {
         $fecha = Carbon::create(2025, 3, 15);
-        $notification = new RecordatorioPresentarDeclaracion($fecha);
+        $notificacion = new RecordatorioPresentarDeclaracion($fecha);
 
-        $mail = $notification->toMail((object) ['nombre' => 'Ana', 'apellido' => 'Soto']);
+        $correo = $notificacion->toMail((object) ['nombre' => 'Ana', 'apellido' => 'Soto']);
 
-        $this->assertInstanceOf(MailMessage::class, $mail);
-        $this->assertSame('Recordatorio: presentación de Declaración Jurada', $mail->subject);
-        $this->assertStringContainsString('Ana Soto', $mail->greeting);
-        $introLines = $mail->introLines;
-        $this->assertStringContainsString('15 de marzo de 2025', end($introLines));
+        $this->assertInstanceOf(MailMessage::class, $correo);
+        $this->assertSame('Recordatorio: presentación de Declaración Jurada', $correo->subject);
+        $this->assertStringContainsString('Ana Soto', $correo->greeting);
+        $lineasIntroductorias = $correo->introLines;
+        $this->assertStringContainsString('15 de marzo de 2025', end($lineasIntroductorias));
     }
 
-    public function test_payload_data_is_available_for_database(): void
+    public function test_los_datos_del_payload_estan_disponibles_para_la_base_de_datos(): void
     {
         $fecha = Carbon::parse('2025-04-01');
-        $notification = new RecordatorioPresentarDeclaracion($fecha);
+        $notificacion = new RecordatorioPresentarDeclaracion($fecha);
 
-        $payload = $notification->toArray((object) []);
+        $datos = $notificacion->toArray((object) []);
 
-        $this->assertSame('Debes presentar una nueva Declaración Jurada.', $payload['message']);
-        $this->assertSame('2025-04-01', $payload['fecha_limite']);
-        $this->assertNotEmpty($payload['action_url']);
+        $this->assertSame('Debes presentar una nueva Declaración Jurada.', $datos['message']);
+        $this->assertSame('2025-04-01', $datos['fecha_limite']);
+        $this->assertNotEmpty($datos['action_url']);
     }
 
-    public function test_channels_include_mail_and_database(): void
+    public function test_los_canales_incluyen_correo_y_base_de_datos(): void
     {
-        $notification = new RecordatorioPresentarDeclaracion();
+        $notificacion = new RecordatorioPresentarDeclaracion();
 
-        $this->assertEquals(['mail', 'database'], $notification->via((object) []));
+        $this->assertEquals(['mail', 'database'], $notificacion->via((object) []));
     }
 }
-
